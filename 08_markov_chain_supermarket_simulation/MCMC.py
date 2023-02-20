e9cc24e0b083ba7acd547c92ad965805ed754e1b
Florian Riemann <flo.riemann@gmail.com>
2023-02-20 21:43:12 +0100

    markov chain

--- 08_markov_chain_supermarket_simulation/MCMC.py
+++
@@ -1,9 +0,0 @@
-import classes.py
-import pandas as pd
-from datetime import datetime, time, timedelta
-
-aldi=Supermarket()
-aldi.simulation()
-
-#save MCMC df to csv
-aldi.df_random_walks.to_csv("MCMC_aldi_result.csv", sep=";")
